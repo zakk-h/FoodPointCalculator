deca6a891d74116c0b3c202251e9aefd7eed04f9
Zakk Heile <63684682+zakk-h@users.noreply.github.com>
2025-08-25 19:28:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -490,14 +490,14 @@ def food_point_calculator():
         "Custom": None
     }
 
-    # Duke seems to be giving an additional 7.5% in food points for the Fall 2024 semester.
+    # Duke seems to be giving an additional amount of food points
     INFLATE = True
 
     # Apply inflation if INFLATE is True
     if INFLATE:
         for key in dining_plans:
             if dining_plans[key] is not None:
-                dining_plans[key] = round(dining_plans[key] * 1.075)
+                dining_plans[key] = round(dining_plans[key] * 1.09)
 
     # Hardcoded term dates and breaks for 2024-2026
     terms = [
